6a9ae548da097718fdee65b35ab4e38d40dd0082
Bae Yeong Sik <yeong0586@gmail.com>
2025-04-25 15:31:01 +0900

    에러타임 수정

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 100000;
 
 app.use(cors());
 app.use(express.json());
